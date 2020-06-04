ac73bb7fc115a5c0c4e62483453bc3aa795c66e4
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2020-06-04 12:10:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,9 @@ setup(
   author = 'Luqmaan Baboo',                   # Type in your name
   author_email = 'luqmaanbaboo@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/xinabox/Python-SW01',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/xinabox/Python-SW01/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['BME280', 'Sensor', 'Temperature', 'Humidity', 'Pressure'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
@@ -23,5 +21,6 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
   ],
 )
